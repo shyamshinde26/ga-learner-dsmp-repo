b6d3f3344a72d778b32d463b4f69315bd3011abb
shyamshinde26 <shyamshinde26@users.noreply.github.com>
2019-10-31 05:05:52 +0530

    Code file created.

---
+++ matplotlib/code.py
@@ -0,0 +1,74 @@
+# --------------
+#Importing header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data=pd.read_csv(path)
+loan_status=data['Loan_Status'].value_counts()
+loan_status.plot(kind='bar', width=0.1)
+plt.title('Loan status chart')
+plt.show()
+
+#Code starts here
+
+
+# --------------
+#Code starts here
+property_and_loan=data.groupby(['Property_Area','Loan_Status']).size().unstack()
+property_and_loan.plot(kind='bar',stacked=False)
+plt.title('Property are and loan status chart')
+plt.xlabel('Property Area')
+plt.ylabel('Loan Status')
+plt.xticks(rotation=45)
+plt.show()
+
+
+# --------------
+#Code starts here
+education_and_loan=data.groupby(['Education','Loan_Status']).size().unstack()
+education_and_loan.plot(kind='bar')
+plt.title('education_and_loan chart')
+plt.xlabel('Education Status')
+plt.ylabel('Loan Status')
+plt.xticks(rotation=45)
+plt.show()
+
+
+# --------------
+#Code starts here
+graduate=data[data['Education']=='Graduate']
+not_graduate=data[data['Education']=='Not Graduate']
+graduate.plot(kind='density',label='Graduate')
+not_graduate.plot(kind='density',label='Not Graduate')
+# plt.title1('density plot for graduates and non-graduates')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+#Code ends here
+
+#For automatic legend display
+plt.legend()
+
+
+# --------------
+#Code starts here
+fig,(ax_1, ax_2, ax_3)=plt.subplots(nrows=3, ncols=1)
+ax_1.scatter(data['ApplicantIncome'],data['LoanAmount'])
+ax_1.set_title('Applicant Income')
+ax_2.scatter(data['CoapplicantIncome'],data['LoanAmount'])
+ax_2.set_title('Coapplicant Income')
+data['TotalIncome']=data['ApplicantIncome']+data['CoapplicantIncome']
+ax_3.scatter(data['TotalIncome'],data['LoanAmount'])
+ax_3.set_title('Total Income')
+
+
+
